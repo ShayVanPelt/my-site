a4dbab58c55911b79d8691669b0c1bb8eed2bfdd
Shay <Shayvanpelt05@gmail.com>
2024-04-30 03:37:41 -0700

    add button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { React, useState } from 'react'
+
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+ 
+  //reset counter 
+  const reset = () =>{
+    setCounter(0)
+  }
+
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
-       
-          Work In Progress...
-          
-           Hi mingo
-        
+        <h1>Coming soon.... in the mean time enjoy this counter</h1>
+        <div className="this">
+          <span className="counter__output">{counter}</span>
+          <button className="control__btn" onClick={increase}>+</button>
+          <button className="control__btn" onClick={decrease}>-</button>
+          <button className="reset" onClick={reset}>Reset</button>
+        </div>
       </header>
     </div>
+    
   );
 }
 
